99fb1f79734ba4936df8bb38d76b0c2aad9e4dec
Simon <simonwoodburyforget@gmail.com>
2019-11-10 08:39:31 -0500

    added missing space

--- src/mindustry-mods.py
+++ src/mindustry-mods.py
@@ -39,7 +39,7 @@ def loads(path):
 def fmt_entry_content(repo, xs):
     def fmt(x):
         link = f"https://raw.githubusercontent.com/{repo}/master/sprites/{x}.png"
-        return f' - ![{x}]({link})'
+        return f'  - ![{x}]({link})'
     
     return "\n".join(fmt(x) for x in xs)
 

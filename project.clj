046aa664a43bfe99a2a56b53ba43148d782d71f4
Simon <simonwoodburyforget@gmail.com>
2019-11-26 00:43:55 -0500

    fixed project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject clodustry "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-
+  :description "Mindustry-Mods listing html generation."
+  :url "https://github.com/SimonWoodburyForget/mindustry-mods"
+  :license {:name "GNU General Public License v3.0"
+            :url "https://github.com/SimonWoodburyForget/mindustry-mods/blob/master/LICENSE"}
+  
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.597"]
                  [reagent "0.9.0-rc3"]

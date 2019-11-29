3d1f824bf890ac407f0fce330193303aa8cbbd0b
Simon <simonwoodburyforget@gmail.com>
2019-11-29 01:26:23 -0500

    Broken scripts.

--- js/app.js
+++ js/app.js
@@ -4,8 +4,5 @@ if(typeof goog == "undefined") document.write('<script src="js/out/goog/base.js"
 document.write('<script src="js/out/goog/deps.js"></script>');
 document.write('<script src="js/out/cljs_deps.js"></script>');
 document.write('<script>if (typeof goog == "undefined") console.warn("ClojureScript could not load :main, did you forget to specify :asset-path?");</script>');
-document.write('<script>goog.require("figwheel.connect");</script>');
 document.write('<script>goog.require("process.env");</script>');
 document.write('<script>goog.require("clodustry.dev");</script>');
-
-document.write("<script>figwheel.connect.start();</script>");

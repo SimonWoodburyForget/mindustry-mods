af13bc683cd8dad3a00b1ce070f62d0d3aea84bb
Simon <simonwoodburyforget@gmail.com>
2020-04-01 06:10:01 -0400

    refactored text out of Escaped variant

--- backend/src/color.rs
+++ backend/src/color.rs
@@ -22,7 +22,7 @@ pub enum ColorTag<'a> {
     Named(&'a str),
 
     /// Parsed escaped color tag.
-    Escaped(&'a str),
+    Escaped,
 
     /// Parsed *pop* current color tag.
     Popped,
@@ -34,18 +34,12 @@ pub enum ColorTag<'a> {
 impl std::fmt::Display for ColorTag<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Escaped(t) => write!(f, "[{}", t),
+            Self::Escaped => write!(f, "["),
             Self::Popped => write!(f, "[]"),
             Self::HexColor { r, g, b, a } => write!(f, "[#{}{}{}{}]", r, g, b, a),
             Self::Named(color) => write!(f, "[{}]", color),
             Self::Text(text) => write!(f, "{}", text),
         }
-    }
-}
-
-impl<'a> ColorTag<'a> {
-    pub fn new(r: u8, g: u8, b: u8, a: u8) -> Self {
-        Self::HexColor { r, g, b, a }
     }
 }
 
@@ -79,7 +73,7 @@ fn hex_color(input: &str) -> PResult<ColorTag> {
         preceded(char('#'), tuple((hex_primary, hex_primary, hex_primary)))(input)?;
     let (input, a) = opt(hex_primary)(input)?;
     let a = a.unwrap_or(0);
-    Ok((input, ColorTag::new(r, g, b, a)))
+    Ok((input, [r, g, b, a].into()))
 }
 
 fn named_color(input: &str) -> PResult<ColorTag> {
@@ -92,8 +86,7 @@ fn color_markup(input: &str) -> PResult<ColorTag> {
         tag("["),
         alt((
             terminated(hex_color, tag("]")),
-            preceded(tag("["), map(is_not("["), |t| ColorTag::Escaped(t))),
-            map(tag("["), |_| ColorTag::Escaped("")),
+            map(tag("["), |_| ColorTag::Escaped),
             map(tag("]"), |_| ColorTag::Popped),
             terminated(named_color, tag("]")),
         )),
@@ -116,39 +109,11 @@ mod test {
 
         #[test]
         fn simple() {
-            assert_eq!(
-                hex_color("#2F14DF"),
-                Ok(("", ColorTag::new(47, 20, 223, 0)))
-            );
-        }
-
-        #[test]
-        fn lower_case() {
-            assert_eq!(
-                hex_color("#2f14df"),
-                Ok(("", ColorTag::new(47, 20, 223, 0)))
-            );
-        }
-
-        #[test]
-        fn mixed_case() {
-            assert_eq!(
-                hex_color("#2F14df"),
-                Ok(("", ColorTag::new(47, 20, 223, 0)))
-            );
-        }
-
-        #[test]
-        fn named() {
+            assert_eq!(hex_color("#2F14DF"), Ok(("", [47, 20, 223].into())));
+            assert_eq!(hex_color("#2f14df"), Ok(("", [47, 20, 223].into())));
+            assert_eq!(hex_color("#2F14df"), Ok(("", [47, 20, 223].into())));
+            assert_eq!(hex_color("#2F14DF05"), Ok(("", [47, 20, 223, 5].into())));
             assert_eq!(named_color("red"), Ok(("", ColorTag::Named("red"))));
-        }
-
-        #[test]
-        fn alpha() {
-            assert_eq!(
-                hex_color("#2F14DF05"),
-                Ok(("", ColorTag::new(47, 20, 223, 5)))
-            );
         }
     }
 
@@ -160,8 +125,7 @@ mod test {
         fn tag() {
             assert_eq!(color_markup("[#010203]"), Ok(("", [1, 2, 3].into())));
             assert_eq!(color_markup("[red]"), Ok(("", Named("red"))));
-            assert_eq!(color_markup("[[e"), Ok(("", Escaped("e"))));
-            assert_eq!(color_markup("[["), Ok(("", Escaped(""))));
+            assert_eq!(color_markup("[["), Ok(("", Escaped)));
             assert_eq!(color_markup("[]"), Ok(("", Popped)));
         }
 
@@ -174,15 +138,18 @@ mod test {
             );
             assert_eq!(
                 markup("[red][[blue[green]"),
-                Ok(("", vec![Named("red"), Escaped("blue"), Named("green")]))
+                Ok((
+                    "",
+                    vec![Named("red"), Escaped, Text("blue"), Named("green")]
+                ))
             );
             assert_eq!(
                 markup("[][[blue[green]"),
-                Ok(("", vec![Popped, Escaped("blue"), Named("green")]))
+                Ok(("", vec![Popped, Escaped, Text("blue"), Named("green")]))
             );
             assert_eq!(
                 markup("[#01020304][[blue"),
-                Ok(("", vec![[1, 2, 3, 4].into(), Escaped("blue")]))
+                Ok(("", vec![[1, 2, 3, 4].into(), Escaped, Text("blue")]))
             );
             assert_eq!(
                 markup("[red]text"),

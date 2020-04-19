5d0bbae350c62bf8044ee0b018d1ae032145ff3d
Simon <simonwoodburyforget@gmail.com>
2020-04-19 14:53:34 -0400

    test

--- backend/src/main.rs
+++
@@ -1,4 +0,0 @@
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    mindustry_mods_backend::main().await
-}
--- scripts/config.py
+++
@@ -1,4 +0,0 @@
-from pathlib import Path
-
-CACHE_DIR = Path.home()/".cache/mindustry-mods/"
-CACHE_DIR.mkdir(parents=True, exist_ok=True)

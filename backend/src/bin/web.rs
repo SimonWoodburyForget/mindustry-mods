c55db8d0bfb6ff7b947b180b5c9dafa4e726b121
simon-wf <simonwoodburyforget@gmail.com>
2020-11-20 09:14:52 -0500

    removed subscript process from webserver

--- backend/src/bin/web.rs
+++ backend/src/bin/web.rs
@@ -12,26 +12,7 @@ async fn server_run() {
     .await;
 }
 
-struct Script {
-    output: std::process::Child,
-}
-
-/// spawn python scripts updating ./dist
-fn spawn_script() -> Script {
-    use std::process::{Command, Stdio};
-
-    let output = Command::new("python3.8")
-        .arg("backend/scripts/mindustry-mods.py")
-        .args(&["-i", "-h"])
-        .stdout(Stdio::null())
-        .spawn()
-        .unwrap();
-
-    Script { output }
-}
-
 #[tokio::main]
 async fn main() {
-    spawn_script();
     server_run().await;
 }
